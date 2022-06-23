5f00d7cb178dde5f6c3b712af4fdd283918882df
Adilson Lima <adilson.pereira.lima@gmail.com>
2022-06-23 20:51:00 -0300

    finished page event

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,16 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+
 import { client } from "./lib/apollo";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Router } from "./Router";
 
 function App() {
-  const { data } = useQuery(GET_LESSONS_QUERY);
-
-  console.log(data);
-
   return (
-    <ul>
-      {data?.lessons.map(({ id, title }: Lesson) => {
-        return <li key={id}>{title}</li>;
-      })}
-    </ul>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
